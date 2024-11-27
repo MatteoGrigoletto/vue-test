9273fc77a8b1c7109f86e2b0dc611d3f5a04bf60
MatteoGrigoletto <110687743+MatteoGrigoletto@users.noreply.github.com>
2024-11-27 21:03:55 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+
+<AppHeader/>
+<AppMain/>
+<AppFooter/>
+
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+  export default {
+    data(){
+      return{
+
+      }
+    },
+    components:{
+      AppHeader,
+      AppMain,
+      AppFooter,
+
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+@import './style/global-scss.scss';
+</style>
